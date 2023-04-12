3d724bf42c8b42ac2ecdbf60d75f2f9918cd6bc6
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2023-04-12 16:30:16 +0200

    refactor: clean up esbuild config, dependencies

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 <p align="center">
-  <img src="logo.png" width="200">
+  <img src="https://user-images.githubusercontent.com/55899582/231488871-e83fb827-1b25-4ec9-a326-b14244677e87.png" width="200">
 </p>
 
 <h1 align="center">Webcrack UI</h1>
--- esbuild.config.js
+++ esbuild.config.js
@@ -1,32 +1,16 @@
 // @ts-check
-
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 import esbuild from 'esbuild';
 
 const args = process.argv.slice(2);
 const watch = args.length > 0 && /^(?:--watch|-w)$/i.test(args[0]);
 
 const ctx = await esbuild.context({
-  entryPoints: [
-    'src/index.ts',
-    'src/style.css',
-    // join(SRC_PATH, 'deobworker.js'),
-  ],
+  entryPoints: ['src/index.ts', 'src/style.css'],
   outdir: 'dist',
 
   bundle: true,
   minify: true,
   format: 'esm',
-
-  plugins: [
-    NodeModulesPolyfillPlugin(),
-    NodeGlobalsPolyfillPlugin({ buffer: true }),
-  ],
-
-  loader: {
-    '.ttf': 'file',
-  },
 
   define: {
     'process.env.NODE_ENV': watch ? '"development"' : '"production"',
