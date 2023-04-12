dfbd9f1705e8f6c380cca33249622ca6d452d38d
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2023-04-12 16:41:31 +0200

    feat: save file

--- src/editor.ts
+++ src/editor.ts
@@ -1,3 +1,4 @@
+import { webcrack } from 'webcrack';
 import { monaco } from './monaco';
 import { PlaceholderContentWidget } from './placeholderWidget';
 import { WebcrackOverlayWidget } from './webcrackWidget';
@@ -13,4 +14,46 @@ export const editor = monaco.editor.create(document.getElementById('editor')!, {
 new PlaceholderContentWidget('// Paste your obfuscated code here', editor);
 new WebcrackOverlayWidget(editor);
 
-editor.focus();
+editor.addAction({
+  id: 'editor.action.webcrack',
+  label: 'Run webcrack',
+  keybindings: [monaco.KeyMod.Alt | monaco.KeyCode.Enter],
+  async run(editor, ...args) {
+    const result = await webcrack(editor.getValue());
+    editor.pushUndoStop();
+    editor.executeEdits('webcrack', [
+      {
+        range: editor.getModel()!.getFullModelRange(),
+        text: result.code,
+      },
+    ]);
+  },
+});
+
+editor.addAction({
+  id: 'editor.action.save',
+  label: 'Save',
+  keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyS],
+  run(editor, ...args) {
+    const code = editor.getValue();
+    if (code === '') return;
+
+    const blob = new Blob([code], {
+      type: 'application/javascript;charset=utf-8',
+    });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+
+    link.setAttribute('href', url);
+    link.setAttribute(
+      'download',
+      `deobfuscated-${new Date().toISOString()}.js`
+    );
+    link.style.display = 'none';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+    URL.revokeObjectURL(url);
+  },
+});
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-import { webcrack } from 'webcrack';
 import { editor } from './editor';
-import { monaco } from './monaco';
 
 // https://esbuild.github.io/api/#live-reload
 if (process.env.NODE_ENV === 'development') {
@@ -9,26 +7,4 @@ if (process.env.NODE_ENV === 'development') {
   );
 }
 
-editor.addAction({
-  id: 'editor.action.webcrack',
-  label: 'Run webcrack',
-  async run(editor, ...args) {
-    const result = await webcrack(editor.getValue());
-    editor.pushUndoStop();
-    editor.executeEdits('webcrack', [
-      {
-        range: editor.getModel()!.getFullModelRange(),
-        text: result.code,
-      },
-    ]);
-  },
-});
-
-editor.addAction({
-  id: 'editor.action.save',
-  label: 'Save',
-  keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyS],
-  run(editor, ...args) {
-    console.log('save');
-  },
-});
+editor.focus();
