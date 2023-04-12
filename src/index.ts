afc85bf59d2b64607a42655842fcb66b28df4268
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2023-04-12 18:03:28 +0200

    feat: add github and bug report icon

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,33 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 editor.focus();
+
+const reportBugButton =
+  document.querySelector<HTMLAnchorElement>('.report-bug')!;
+
+reportBugButton.addEventListener('click', event => {
+  event.preventDefault();
+
+  const template = `
+## Describe the bug
+
+<!-- Clear and concise description of what the bug is -->
+
+## Expected behaviour
+
+<!-- Clear and concise description of what you expected to happen -->
+
+## Input Code
+
+\`\`\`js
+${editor.getValue()}
+\`\`\`
+`;
+
+  window.open(
+    `https://github.com/j4k0xb/webcrack/issues/new?body=${encodeURIComponent(
+      template
+    )}`,
+    '_blank'
+  );
+});
