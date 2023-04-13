00128c875e8058e444188cad96bb3a991579b8a0
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2023-04-13 19:14:23 +0200

    feat: use github issue forms url

--- src/index.ts
+++ src/index.ts
@@ -34,24 +34,3 @@ worker.onmessage = ({ data }) => {
     alert(data.error);
   }
 };
-
-const reportBugButton =
-  document.querySelector<HTMLAnchorElement>('.report-bug')!;
-
-const issueTemplate = `## Describe the bug
-
-<!-- Clear and concise description of what the bug is -->
-
-## Expected behaviour
-
-<!-- Clear and concise description of what you expected to happen -->
-
-## Input Code
-
-\`\`\`js
-
-\`\`\``;
-
-reportBugButton.href = `https://github.com/j4k0xb/webcrack/issues/new?body=${encodeURIComponent(
-  issueTemplate
-)}`;
